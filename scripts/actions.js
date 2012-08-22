11c21ab7dad1a90a7544b8209b7adfcc9120e29f
Niklas Brunberg <n.brunberg@gmail.com>
2012-08-22 16:29:20 +0200

    Nivo slider starter nu, samt flytta user scripts till sist

---
+++ scripts/actions.js
@@ -0,0 +1,9 @@
+
+// http://nivo.dev7studios.com/support/jquery-plugin-usage/
+$(window).load(function() {
+    $('#slider').nivoSlider({
+        prevText: 'Prev', // Prev directionNav text
+        nextText: 'Next', // Next directionNav text
+        controlNav: false // 1,2,3... navigation
+    });
+});
