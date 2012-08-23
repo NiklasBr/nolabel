cc53f8078a798b02a356ff83f2b199c63d22865d
Niklas Brunberg <n.brunberg@gmail.com>
2012-08-23 10:59:24 +0200

    Fix för avsaknad av stöd för Media Queries i IE8

--- scripts/actions.js
+++ scripts/actions.js
@@ -12,6 +12,22 @@ $(window).load(function() {
     });
 });
 
+// För att hjälpa IE8 att visa media queries
+$(window).resize(function() {
+    if(this.resizeTO) clearTimeout(this.resizeTO);
+    this.resizeTO = setTimeout(function() {
+        $(this).trigger('resizeEnd');
+    }, 500);
+});
+$(window).bind('resizeEnd', function() {
+    if ($(window).width() >= 1600) {
+        $("body").addClass("ie-1600").removeClass("ie-320");
+    } else if ($(window).width() <= 767) {
+        $("body").addClass("ie-320").removeClass("ie-1600");
+    } else {
+        $("body").removeClass("ie-320").removeClass("ie-1600");
+    }
+});
 
 // Bättra på språknavigationen genom att kopiera in länkens text till title-attribut i det synliga li-elementet
 $(document).ready(function() {
