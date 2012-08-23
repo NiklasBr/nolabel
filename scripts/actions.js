18d277e73f038a36930b487222854b7d7775e830
Niklas Brunberg <n.brunberg@gmail.com>
2012-08-23 08:10:55 +0200

    Nivo Solider, inställningar för effekt, animationshastighet och paus-tid

--- scripts/actions.js
+++ scripts/actions.js
@@ -2,6 +2,9 @@
 // http://nivo.dev7studios.com/support/jquery-plugin-usage/
 $(window).load(function() {
     $('#slider').nivoSlider({
+        effect: 'fold', // Specify sets like: 'fold,fade,sliceDown'
+        animSpeed: 750, // Slide transition speed
+        pauseTime: 5000, // How long each slide will show
         prevText: 'Prev', // Prev directionNav text
         nextText: 'Next', // Next directionNav text
         controlNav: false, // 1,2,3... navigation
